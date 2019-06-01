17a75749aa9ddefe46874ae343498203424cd4e4
= <raccoon_js@protonmail.com>
2019-05-31 22:30:27 -0500

    menu screen 90% done

--- game/scripts/menu.gd
+++ game/scripts/menu.gd
@@ -1,14 +1,52 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var red = Color("#E3C5C1")
+var deep_red = Color("#A26D66")
+var green = Color("#C1E3D6")
+var deep_green = Color("#419482")
+var blue = Color("#C1DEE2")
+var deep_blue = Color("#417D95")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$placeholder_text/animation.play("pop")
+	normal_color()
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func normal_color():
+	$bg_and_title/background.color = blue
+	$bg_and_title/title.modulate = deep_blue
+	$button_space/buttons.modulate = deep_blue
+	pass
+
+func _on_quit_mouse_entered():
+	$bg_and_title/background.color = red
+	$bg_and_title/title.modulate = deep_red
+	$button_space/buttons.modulate = deep_red
+	pass # Replace with function body.
+
+func _on_quit_mouse_exited():
+	normal_color()
+	pass # Replace with function body.
+
+
+func _on_start_mouse_entered():
+	$bg_and_title/background.color = green
+	$bg_and_title/title.modulate = deep_green
+	$button_space/buttons.modulate = deep_green
+	pass # Replace with function body.
+
+func _on_start_mouse_exited():
+	normal_color()
+	pass # Replace with function body.
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+func _on_start_pressed():
+	print("start the story")
+	pass # Replace with function body.
+
+func _on_credits_pressed():
+	print("to the credits")
+	pass # Replace with function body.
