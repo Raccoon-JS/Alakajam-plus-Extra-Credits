596b9b157e667af4ef2d3579676c29b85c78fb97
= <raccoon_js@protonmail.com>
2019-06-02 16:23:06 -0500

    making progress

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -5,9 +5,15 @@ func _ready():
 		if global.character == "Jesse":
 			$background.color = global.red
 			$story.texture = load("res://images/Jesse_eye_red.jpg")
+			$left_side/materials/time_and_points/time.modulate = global.deep_red
+			$left_side/materials/time_and_points/points.modulate = global.deep_red
+			$left_side/materials/keys.modulate = global.deep_red
 		elif global.character == "James":
 			$background.color = global.green
 			$story.texture = load("res://images/James_eye_green.jpg")
+			$left_side/materials/time_and_points/time.modulate = global.deep_green
+			$left_side/materials/time_and_points/points.modulate = global.deep_green
+			$left_side/materials/keys.modulate = global.deep_green
 		else:
 			$background.color = global.blue # To avoid error
 	elif name == "2":
