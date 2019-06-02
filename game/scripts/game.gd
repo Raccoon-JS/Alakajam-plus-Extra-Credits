ad1152cc0a9bee8dbdbf153b969c098d7fa2937a
= <raccoon_js@protonmail.com>
2019-06-02 16:00:14 -0500

    resized some images

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -4,8 +4,10 @@ func _ready():
 	if name == "1":
 		if global.character == "Jesse":
 			$background.color = global.red
+			$story.texture = load("res://images/Jesse_eye_red.jpg")
 		elif global.character == "James":
 			$background.color = global.green
+			$story.texture = load("res://images/James_eye_green.jpg")
 		else:
 			$background.color = global.blue # To avoid error
 	elif name == "2":
